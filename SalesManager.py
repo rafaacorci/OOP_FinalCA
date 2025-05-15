e3eb72810a6f18da4bc3d6537d79db65d406253d
Accorci <rafael.accorci1@gmail.com>
2025-05-15 18:30:51 +0100

    coded and tested the option to generate sales bar plots per day by type of item that was sold

--- SalesManager.py
+++ SalesManager.py
@@ -2,6 +2,7 @@ from datetime import date
 import pandas as pd
 from constructors import Sales
 from InventoryManager import InventoryManager #importing so I can call some methods from that class
+import matplotlib.pyplot as plt
 
 
 class SalesManager:
@@ -14,97 +15,57 @@ class SalesManager:
     def load_sales(self):
         self.sales_df = pd.read_csv("sales.csv")
     # method to record a new sale
-    def record_sales_bicyles(self):
+    def record_sales_bicyles(self, item_id):
         self.load_sales()
         sale_id = len(self.sales_df)+1
         #opening the bicycle CSV file
         self.inventory.load_bicycles()
         bike_df = self.inventory.bicycles_df
-        while True:
-            try:
-                item_id = int(input("What is the item ID?"))
-            except ValueError:
-                    print("Invalid input, try again.")
-                    continue
-                    #checking if the item_id entered is valid
-            if item_id not in bike_df["item_id"].values:
-                    print("Item not found not found. Would you like to try again?")
-                    while True:
-                        #since the input is case sensitive, I'm making sure that the input is always lowercase
-                        user_choice = input("Y/N: ").strip().lower()
-                        if user_choice == "y":
-                                break  # return to the previous loop
-                        elif user_choice == "n":
-                                print("Thank you, have a nice day")
-                                return  # adicionar opcao depois para que o usuario volte para o menu (agora eu to saindo por competo)
-                        else:
-                            print("Invalid input, try again")
-                            continue
-            else:
-                # I will use the information stored in the bicycle dateframe to build the the sales constructor
-                bike_id = bike_df[bike_df["item_id"] == item_id].iloc[0]
-                #getting the information I want from the dataframe and preparing for the constructor
-                brand = bike_id["brand"]
-                model = bike_id["model"]
-                price = bike_id["price"]
-                sale_date = date.today()
-                #building the sales constructor
-                new_sale = Sales(sale_id,item_id,"Bicycle",None,brand,model,sale_date,price)
-                #converting to a dictionary to so it can be added to the Dataframe
-                sale_data = {
-                            "sale_id":new_sale.sale_id,
-                            "item_id":new_sale.item_id,
-                            "kind":new_sale.kind,
-                            "category":new_sale.category,
-                            "brand":new_sale.brand,
-                            "model":new_sale.model,
-                            "sale_date":new_sale.sale_date,
-                            "price":new_sale.price
-                            }
-                #concatenating to the Dataframe
-                self.sales_df = pd.concat([self.sales_df,pd.DataFrame([sale_data])],ignore_index=True)
-                #saving the data to the respective CSV file
-                self.sales_df.to_csv("sales.csv", index = False)
-                print(f"Your bicycle {item_id} sale has been recorded as sale_id {sale_id}")
-                return
-    def record_sales_spare(self):
+        # I will use the information stored in the bicycle dateframe to build the the sales constructor
+        bike_id = bike_df[bike_df["item_id"] == item_id].iloc[0]
+        #getting the information I want from the dataframe and preparing for the constructor
+        brand = bike_id["brand"]
+        model = bike_id["model"]
+        price = bike_id["price"]
+        sale_date = date.today()
+        #building the sales constructor
+        new_sale = Sales(sale_id,item_id,"Bicycle",None,brand,model,sale_date,price)
+        #converting to a dictionary to so it can be added to the Dataframe
+        sale_data = {
+                    "sale_id":new_sale.sale_id,
+                    "item_id":new_sale.item_id,
+                    "kind":new_sale.kind,
+                    "category":new_sale.category,
+                    "brand":new_sale.brand,
+                    "model":new_sale.model,
+                    "sale_date":new_sale.sale_date,
+                    "price":new_sale.price
+                    }
+        #concatenating to the Dataframe
+        self.sales_df = pd.concat([self.sales_df,pd.DataFrame([sale_data])],ignore_index=True)
+        #saving the data to the respective CSV file
+        self.sales_df.to_csv("sales.csv", index = False)
+
+    #record the sales of spare parts
+    def record_sales_spare(self, item_id):
         self.load_sales()
         sale_id = len(self.sales_df) +1
         #opening spare parts CSV
         self.inventory.load_spares()
         spare_df = self.inventory.spares_df
-        while True:
-            try:
-                item_id = int(input("What is the item ID?"))
-            except ValueError:
-                print("Invalid input. Try again.")
-                continue
-            if item_id not in spare_df["item_id"].values:
-                print("Item not found. Would you like to try again")
-                while True:
-                    # since the input is case sensitive, I'm making sure that the input is always lowercase
-                    user_choice = input("Y/N: ").strip().lower()
-                    if user_choice == "y":
-                        break #return to the previous loop
-                    elif user_choice =="n":
-                        print("Have a great day")
-                        return #exiting the method
-                    else:
-                        print("Invalid input. Try again")
-                        continue
-            else:
-                #using this information to help build the constructor
-                part_id = spare_df[spare_df["item_id"]==item_id].iloc[0]
-                #fetching the information I want from the DataFrame to build the constructor
-                brand = part_id["brand"]
-                model = part_id["model"]
-                price = part_id["price"]
-                category = part_id["category"]
-                sale_date = date.today()
-                #building the contructor
-                new_sale = Sales(sale_id,item_id,"SparePart",category,brand,model,sale_date,price)
-                #converting the instance to a dictionary so it can be added to the DataFrame and CSV
-                sale_data = {
+        item_id = int(input("What is the item id?"))
+        #using this information to help build the constructor
+        part_id = spare_df[spare_df["item_id"]==item_id].iloc[0]
+        #fetching the information I want from the DataFrame to build the constructor
+        brand = part_id["brand"]
+        model = part_id["model"]
+        price = part_id["price"]
+        category = part_id["category"]
+        sale_date = date.today()
+        #building the contructor
+        new_sale = Sales(sale_id,item_id,"SparePart",category,brand,model,sale_date,price)
+        #converting the instance to a dictionary so it can be added to the DataFrame and CSV
+        sale_data = {
                     "sale_id":new_sale.sale_id,
                     "item_id":new_sale.item_id,
                     "kind":new_sale.kind,
@@ -114,22 +75,30 @@ class SalesManager:
                     "sale_date":new_sale.sale_date,
                     "price":new_sale.price
                 }
-                #concatenating to the DataFrame
-                self.sales_df = pd.concat([self.sales_df,pd.DataFrame([sale_data])], ignore_index= True)
-                #saving the data to the respective CSV file.
-                self.sales_df.to_csv("sales.csv", index = False)
-                print(f"Your spare part {item_id} sale has been recorded as sale_id {sale_id}")
-                return
-
-
-
-
-
+        #concatenating to the DataFrame
+        self.sales_df = pd.concat([self.sales_df,pd.DataFrame([sale_data])], ignore_index= True)
+        #saving the data to the respective CSV file.
+        self.sales_df.to_csv("sales.csv", index = False)
 
     # method to save the changes made on the sales CSV file
     def save_sales(self):
         pass
     # method generate sales reports
     def generate_sales_report(self):
-        pass
+        self.load_sales()
+        #grouping the sales by dates and kinds with the sum of the total each sold.
+        self.sales_df["sale_date"] = pd.to_datetime(self.sales_df["sale_date"])
+        grouped_sales = (self.sales_df.groupby([self.sales_df["sale_date"].dt.date,"kind"])["price"].sum().unstack())
+        #plotting bar chart
+        grouped_sales.plot(kind = "bar", figsize=(12,6))
+        plt.title("Total sales revenue of bicycles and spare parts per day")
+        plt.xlabel("Sale Date")
+        plt.ylabel("Total sales €")
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.legend(title="Kind")
+        plt.show()
+
+
+
 
